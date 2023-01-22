2ba54ce76ff5a536f320a33f9ba0dd1f42c254cb
Daniel Karath <karath.dani@gmail.com>
2023-01-22 18:58:21 +0100

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Daniel Karath on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
